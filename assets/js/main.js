435e5d036b3c4446d6e7977e18de3e0bfd8aa2e6
SenthamizhanV <sendhucool@gmail.com>
2023-03-06 21:07:02 +0530

    Added a LOOOTTT of content

--- assets/js/main.js
+++ assets/js/main.js
@@ -201,6 +201,42 @@
   });
 
   /**
+   * AI Partners Slider
+   */
+   new Swiper('.partners-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 40
+      },
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 60
+      },
+      640: {
+        slidesPerView: 3,
+        spaceBetween: 80
+      },
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 120
+      }
+    }
+  });
+
+  /**
    * Porfolio isotope and filter
    */
   window.addEventListener('load', () => {
